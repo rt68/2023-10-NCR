8447b32e55df846909f7c71d4b6ab45d7ee7186f
rt68 <54224071+rt68@users.noreply.github.com>
2024-01-10 10:14:31 -0500

    redux lesson

---
+++ unit_2/reduxlesson/src/features/counter/Counter.js
@@ -0,0 +1,26 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+// import styles from './Counter.module.css'
+
+export default function Counter() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+  )
+}
