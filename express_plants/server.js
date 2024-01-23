a3d0c8a9758365f55ff52456e303869b35631515
rt68 <54224071+rt68@users.noreply.github.com>
2024-01-23 11:16:33 -0500

    learn express

---
+++ express_plants/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const plants = ['Monstera Deliciosa', 'Corpse Flower', 'Elephant-Foot Yam', "Witches' Butter",];
+
+app.get('/awesome', (req, res) => { //this will never be reached
+    res.send(`
+      <h1>Plants are awesome!</h1>
+      <img src="https://static.boredpanda.com/blog/wp-content/uuuploads/plant-sculptures-mosaicultures-internationales-de-montreal/plant-sculptures-mosaicultures-internationales-de-montreal-14.jpg" >
+    `);
+  });
+  app.get('/hello/:firstname/:lastname', (req, res) => {
+	console.log(req.params);
+    console.log(req.query);
+	res.send('hello ' + req.query.title + ' ' + req.params.firstname + ' ' + req.params.lastname);
+});
+app.get('/:indexOfPlantsArray', (req, res) => {
+    if (plants[req.params.index]) {
+        res.send(plants[req.params.indexOfPlantsArray]);
+  } else {
+    res.send('cannot find anything at this index: ' + req.params.indexOfPlantsArray);
+  }
+});
+app.get('/awesome', (req, res) => { //this will never be reached
+    res.send(`
+      <h1>Plants are awesome!</h1>
+      <img src="https://static.boredpanda.com/blog/wp-content/uuuploads/plant-sculptures-mosaicultures-internationales-de-montreal/plant-sculptures-mosaicultures-internationales-de-montreal-14.jpg" >
+    `);
+  });
+
+app.listen(port,() => {
+    console.log('listening on port' , port);
+});
