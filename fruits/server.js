6f3f1a0097defd38f8099ac6e3a46a9395fac0a8
rt68 <54224071+rt68@users.noreply.github.com>
2024-01-26 12:43:24 -0500

    fruit induces

--- fruits/server.js
+++ fruits/server.js
@@ -3,11 +3,37 @@ const app = express();
 const fruits = require('./models/fruits')
 
 app.set('view engine', 'jsx');
-  app.engine('jsx', require('express-react-views').createEngine());
-
-  app.get('/fruits', function(req, res){
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.urlencoded({extended:false}));
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+//Induces
+//Index
+app.get('/fruits', function(req, res){
     res.render('Index', { fruits: fruits });
 });   
+
+//New
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+});
+//Delete
+//Update
+//Create
+app.post('/fruits', (req, res) => {
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    fruits.push(req.body);
+    console.log(fruits);
+    res.redirect('/fruits'); //send the user back to /fruits
+});
+//Edit
+//Show
 //add show route
 app.get('/fruits/:index', (req, res) => {
      res.render('Show', { //second param must be an object
--- fruits/views/Index.jsx
+++ fruits/views/Index.jsx
@@ -6,10 +6,13 @@ class Index extends React.Component {
       return (
               <div>
                   <h1>Fruits Index Page</h1>
+                  <nav>
+                    <a href="/fruits/new">Create new fruit</a>
+                  </nav>
                   <ul>
                       {fruits.map((fruit, i) => {
                           return (
-                              <li>
+                              <li key={i}>
                                   The{' '}
                                   <a href={`/fruits/${i}`}>
                                       {fruit.name}
