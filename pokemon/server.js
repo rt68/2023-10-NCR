5e89bc7bea71a0fb75004da8921a25d64d69a2cc
rt68 <54224071+rt68@users.noreply.github.com>
2024-01-29 13:16:01 -0500

    Connected my database, can see json in the browser

--- pokemon/server.js
+++ pokemon/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pokemon = require('./models/pokemon')
 
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
@@ -9,7 +10,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 //INDUCES
-
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
+})
 
 app.listen(port, () => {
     console.log('listening on 3000');
