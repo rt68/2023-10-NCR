73235cdb4b416987fd00c068f0d3d71a6a1127a5
rt68 <54224071+rt68@users.noreply.github.com>
2024-02-07 09:37:04 -0500

    comments schema

--- captains_log/models/logs.js
+++ captains_log/models/logs.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Define the Comment Schema as a subdocument
+const commentSchema = new Schema({
+  name: { type: String, required: true },
+  text: { type: String, required: true },
+}, {
+  timestamps: true // Adds createdAt and updatedAt timestamps to each comment
+});
+
 // Define the Logs Schema
 const logsSchema = new Schema({
   title: { type: String, required: true },
   entry: { type: String, required: true },
-  shipIsBroken: { type: Boolean, default: true }
+  shipIsBroken: { type: Boolean, default: true },
+  comments: [commentSchema] //Embedding the comment schema
 }, {
   timestamps: true // Super bonus: Adds createdAt and updatedAt timestamps to documents
 });
